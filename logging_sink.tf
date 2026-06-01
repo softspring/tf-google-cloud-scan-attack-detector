e86e17d536115271d94e60e3447d1c13230173ff
Javi H. Gil <javihgil@softspring.es>
2026-06-01 11:12:58 +0200

    Grant logging sink publish access

--- logging_sink.tf
+++ logging_sink.tf
@@ -5,6 +5,13 @@ resource "google_logging_project_sink" "notfound_sink" {
   filter      = var.sink_filter
 }
 
+resource "google_pubsub_topic_iam_member" "notfound_sink_publisher" {
+  project = var.project
+  topic   = google_pubsub_topic.income_notfound_request.name
+  role    = "roles/pubsub.publisher"
+  member  = google_logging_project_sink.notfound_sink.writer_identity
+}
+
 resource "google_pubsub_topic" "income_notfound_request" {
   project = var.project
   name    = "${var.resource_prefix}-income-notfound-request"
